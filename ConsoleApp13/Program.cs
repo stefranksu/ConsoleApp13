5512c575a3a6f8899c5182458cf64f416aecc4d4
Frank STE <stefranksu@gmail.com>
2020-03-13 09:29:19 +0800

    v1.0.20073.7382

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -9,7 +9,18 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("V1.1.1");
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            Console.WriteLine(version); // -> "1.1.2.10"
+
+            //var assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            //var fileVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(assemblyLocation).FileVersion;
+            //Console.WriteLine(fileVersion); // -> "1.2.32.2"
+
+            //var assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            //var version = System.Diagnostics.FileVersionInfo.GetVersionInfo(assemblyLocation).ProductVersion;
+            //Console.WriteLine(version); // -> "1.2.32-alpha-01"
+
+
         }
     }
 }
